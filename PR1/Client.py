7891005a8398d65d1159b0ccf47063c872fc6118
VedantDhamale <vedantdhamale9@gmail.com>
2025-01-16 12:47:53 +0530

    minor changes

--- PR1/Client.py
+++ PR1/Client.py
@@ -1,5 +1,5 @@
 import xmlrpc.client
 n=int(input('Enter the value of n:'))
 with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
-    print("Printing: %s" % str(proxy.print_hello('Atharva')))
+    print("Printing: %s" % str(proxy.print_hello('Vedant')))
     print(f"Fatcorial of {n}: %s" % str(proxy.Factorial(n)))
